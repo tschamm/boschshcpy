3a0d6ea10bf2d81256f5abc51aa0342e10c0102a
Thomas Schamm <thomas@tschamm.de>
2020-04-05 08:41:53 +0200

    Increased version number to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="boschshcpy",
-    version="0.1.1-dev",
+    version="0.1.1",
     url="https://github.com/tschamm/boschshcpy",
     author="Clemens-Alexander Brust, Thomas Schamm",
     author_email="cabrust@pm.me, thomas@tschamm.de",
