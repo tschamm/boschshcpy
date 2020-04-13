068175a8c017a50cc3f7ad07d321f3c4eb25ea00
Thomas Schamm <thomas@tschamm.de>
2020-04-13 12:28:06 +0200

    Expose SHCThermostat device model.

--- boschshcpy/__init__.py
+++ boschshcpy/__init__.py
@@ -1,5 +1,6 @@
 from .device_helper import (SHCCameraEyes, SHCDeviceHelper,
                             SHCIntrusionDetectionSystem, SHCShutterContact,
-                            SHCShutterControl, SHCSmartPlug, SHCSmokeDetector)
+                            SHCShutterControl, SHCSmartPlug, SHCSmokeDetector,
+                            SHCThermostat)
 from .scenario import SHCScenario
 from .session import SHCSession
