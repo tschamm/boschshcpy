91df3ecae07db91a01e1c9fd682ae84640a1cbb7
Markus Beil <mb@beil-dev.de>
2023-01-09 21:39:10 +0100

    changed version

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
 
 setup(
     name="boschshcpy",
-    version="0.2.37",
-    url="https://github.com/tschamm/boschshcpy",
+    version="0.2.37.1",
+    url="https://github.com/beilm/boschshcpy",
     author="Clemens-Alexander Brust, Thomas Schamm",
     author_email="cabrust@pm.me, thomas@tschamm.de",
     description="Bosch Smart Home Controller API Python Library",
