21b8885dca3aac187a8bb60daf800348f54f3021
Thomas Schamm <thomas@tschamm.de>
2023-01-22 16:57:23 +0100

    Make registration of new clients executable.

--- boschshcpy/register_client.py
+++ boschshcpy/register_client.py
@@ -2,6 +2,7 @@ import base64
 import json
 import logging
 import os.path
+import sys
 
 import pkg_resources
 import requests
@@ -22,7 +23,7 @@ class HostNameIgnoringAdapter(HTTPAdapter):
 
 
 class SHCRegisterClient:
-    """ Press and hold the button at the front of the SHC until the lights are blinking before you POST the command. When the SHC is not in pairing mode, there will be a connection error."""
+    """Press and hold the button at the front of the SHC until the lights are flashing before you POST the command. When the SHC is not in pairing mode, there will be a connection error."""
 
     def __init__(self, controller_ip: str, password: str):
         """Initializes with IP address and access credentials."""
@@ -60,7 +61,7 @@ class SHCRegisterClient:
                 return {}
         except requests.exceptions.SSLError as e:
             raise SHCRegistrationError(
-                f"SHC probably not in pairing mode! Please press the Bosch Smart Home Controller button until LED starts blinking.\n(SSL Error: {e})."
+                f"SHC probably not in pairing mode! Please press the Bosch Smart Home Controller button until LED starts flashing.\n(SSL Error: {e})."
             )
 
     def _process_nok_result(self, result):
@@ -68,39 +69,21 @@ class SHCRegisterClient:
             f"API call returned non-OK result (code {result.status_code})!: {result.content}... Please check your password?"
         )
 
-    def register(self, client_id, name, certificate=None):
-        cert = key = None
-        if not certificate:
-            cert, key = generate_certificate(client_id, name)
-            certstr = (
-                cert.decode("utf-8")
-                .replace("\n", "")
-                .replace("-----BEGIN CERTIFICATE-----", "-----BEGIN CERTIFICATE-----\r")
-                .replace("-----END CERTIFICATE-----", "\r-----END CERTIFICATE-----")
-            )
-
-        else:
-            if not os.path.exists(certificate):
-                logger.error("No valid cert file, aborting!")
-                # only continue if path to certificate is valid
-                return None
-
-            with open(certificate, "r") as file:
-                certstr = (
-                    file.read()
-                    .replace("\n", "")
-                    .replace(
-                        "-----BEGIN CERTIFICATE-----", "-----BEGIN CERTIFICATE-----\r"
-                    )
-                    .replace("-----END CERTIFICATE-----", "\r-----END CERTIFICATE-----")
-                )
+    def register(self, client_id, name):
+        cert, key = generate_certificate(client_id, name)
+        cert_str = (
+            cert.decode("utf-8")
+            .replace("\n", "")
+            .replace("-----BEGIN CERTIFICATE-----", "-----BEGIN CERTIFICATE-----\r")
+            .replace("-----END CERTIFICATE-----", "\r-----END CERTIFICATE-----")
+        )
 
         data = {
             "@type": "client",
             "id": client_id,
             "name": f"oss_{name}_Binding",
             "primaryRole": "ROLE_RESTRICTED_CLIENT",
-            "certificate": certstr,
+            "certificate": cert_str,
         }
 
         result = self._post_api_or_fail(data)
@@ -109,3 +92,80 @@ class SHCRegisterClient:
             if "token" in result
             else None
         )
+
+
+def write_tls_asset(filename: str, asset: bytes) -> None:
+    """Write the tls assets to disk."""
+    with open(filename, "w", encoding="utf8") as file_handle:
+        file_handle.write(asset.decode("utf-8"))
+
+
+def main():
+    import argparse, sys
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-pw",
+        "--password",
+        help="system password which was set-up initially in the SHC setup process.",
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        help="Name of the new client user (default: BOSCHSHCPY_Client)",
+        default="BOSCHSHCPY_Client",
+    )
+    parser.add_argument(
+        "-id",
+        "--id",
+        help="ID of the new client user (default: boschshcpy_client)",
+        default="boschshcpy_client",
+    )
+    parser.add_argument("-ip", "--ip_address", help="IP of the smart home controller.")
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+
+    # Create a BoschSHC client with the specified ACCESS_CERT and ACCESS_KEY.
+    helper = SHCRegisterClient(args.ip_address, args.password)
+    result = None
+    try:
+        result = helper.register(args.id, args.name)
+    except SHCRegistrationError as e:
+        print(e)
+
+    if result != None:
+        print("successful registered new device with token {}".format(result["token"]))
+        print(f"Cert: {result['cert']}")
+        print(f"Key: {result['key']}")
+        if args.access_cert is None:
+            hostname = result["token"].split(":", 1)[1]
+            print(
+                f"Create new certificate key pair: {'oss_' + args.id + '_' + hostname + '_cert.pem'} {'oss_' + args.id + '_' + hostname + '_key.pem'}"
+            )
+            write_tls_asset(
+                "oss_" + args.id + "_" + hostname + "_cert.pem", result["cert"]
+            )
+            write_tls_asset(
+                "oss_" + args.id + "_" + hostname + "_key.pem", result["key"]
+            )
+
+    else:
+        print(
+            "No valid token received. Did you press client registration button on smart home controller?"
+        )
+        sys.exit()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- setup.py
+++ setup.py
@@ -1,19 +1,29 @@
 from setuptools import find_packages, setup
+import pathlib
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="boschshcpy",
-    version="0.2.43",
+    version="0.2.44",
     url="https://github.com/tschamm/boschshcpy",
     author="Clemens-Alexander Brust, Thomas Schamm",
     author_email="cabrust@pm.me, thomas@tschamm.de",
     description="Bosch Smart Home Controller API Python Library",
+    keywords="boschshcpy",
     license="bsd-3-clause",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "boschshc_registerclient=boschshcpy.register_client:main",
+        ],
+    },
+    package_data={"": ["tls_ca_chain.pem"]},
     classifiers=[
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
@@ -21,11 +31,16 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10, <4",
     install_requires=[
         "cryptography>=3.3.2",
         "getmac>=0.8.2,<1",
         "requests>=2.22",
         "zeroconf>=0.28.0",
     ],
+    project_urls={
+        "Bug Reports": "https://github.com/tschamm/boschshcpy/issues",
+        "Source": "https://github.com/tschamm/boschshcpy",
+    },
+    include_package_data=True,
 )
