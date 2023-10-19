054e61880fce710fb32dd55b08ca84950f4a3b74
Thomas Schamm <thomas@tschamm.de>
2023-10-19 21:57:50 +0200

    Make polling example work again

--- examples/pollingtest.py
+++ examples/pollingtest.py
@@ -1,70 +1,53 @@
-#!/usr/bin/env python
-
-import sys, os 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import time
+import os
+import sys, time
+import zeroconf
 import logging
+from zeroconf import Zeroconf
 
-import BoschShcPy
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-ACCESS_CERT = 'keystore/aps-cert.pem'
-ACCESS_KEY = 'keystore/aps-key.pem'
+from boschshcpy import SHCSession, SHCDevice
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
-IP_SHC = '192.168.1.6'
-PORT_SHC = '8444'
 
-def callback(device):
-    print("Notification of device update:")
-    print(device)
+def callback():
+    print("Notification of device update")
 
-def setup_client():
-    try:
-        # Create a BoschSHC client with the specified ACCESS_CERT and ACCESS_KEY.
-        client = BoschShcPy.Client(IP_SHC, PORT_SHC, ACCESS_CERT, ACCESS_KEY)
 
-        shc_info = client.shc_information()
-        print('  version        : %s' % shc_info.version)
-        print('  updateState    : %s' % shc_info.updateState)
+def setup_session():
+    # Create a BoschSHC client with the specified ACCESS_CERT and ACCESS_KEY.
+    zeroconf = Zeroconf()
+    session = SHCSession(
+        controller_ip="192.168.1.6",
+        certificate="../keystore/dev-cert.pem",
+        key="../keystore/dev-key.pem",
+        zeroconf=zeroconf,
+    )
+    zeroconf.close()
 
-        #   device = client.device(shutter_control_ID)
-        #   print(device)
-        #
+    shc_info = session.information
+    print("  version        : %s" % shc_info.version)
+    print("  updateState    : %s" % shc_info.updateState)
 
-        print("Accessing all devices...")
+    print("Accessing all devices...")
 
-        smart_plugs = BoschShcPy.smart_plug.initialize_smart_plugs(client, client.device_list())
-        for item in smart_plugs:
-            client.register_device(item, callback)
-            client.register_device(item.device, callback)
+    smart_plugs = session.device_helper.smart_plugs
+    for item in smart_plugs:
+        for service in item.device_services:
+            service.subscribe_callback(item.id, callback)
 
-        shutter_controls = BoschShcPy.shutter_control.initialize_shutter_controls(client, client.device_list())
-        for item in shutter_controls:
-            client.register_device(item, callback)
-            client.register_device(item.device, callback)
+    shutter_controls: SHCDevice = session.device_helper.shutter_controls
+    for item in shutter_controls:
+        for service in item.device_services:
+            service.subscribe_callback(item.id, callback)
 
-        shutter_contacts = BoschShcPy.shutter_contact.initialize_shutter_contacts(client, client.device_list())
-        for item in shutter_contacts:
-            client.register_device(item, callback)
-            client.register_device(item.device, callback)
+    return session
 
-        # intrusion_detection = BoschShcPy.IntrusionDetection(client)
-        # intrusion_detection.register_polling(client, callback)
-
-    except BoschShcPy.client.ErrorException as e:
-        print('\nAn error occured while requesting a SmartPlug object:\n')
-
-        for error in e.errors:
-            print('  code        : %d' % error.code)
-            print('  description : %s' % error.description)
-            print('  parameter   : %s\n' % error.parameter)
-
-    return client
 
 def main():
     exit = False
-    client = setup_client()
+    session = setup_session()
     duration = 0
     while not exit:
         userInput = input("Enter seconds for polling duration (or nothing to exit) ")
@@ -72,13 +55,14 @@ def main():
         if userInput.isdigit():
             duration = int(userInput)
             print("Starting polling for {} seconds".format(duration))
-            client.start_subscription()
+            session.start_polling()
             time.sleep(duration)
-            client.stop_subscription()
+            session.stop_polling()
             print("Stopping polling")
         else:
             exit = True
             print("OK Bye, bye.")
+
 
 if __name__ == "__main__":
     main()
