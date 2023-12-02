1445d63264bc24ec5f1ecc99ffca24eae5664e77
Thomas Schamm <thomas@tschamm.de>
2023-12-02 19:49:37 +0100

    Update for apisummary

--- examples/apisummary.py
+++ examples/apisummary.py
@@ -4,16 +4,25 @@ import logging
 import zeroconf
 from zeroconf import Zeroconf
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
-from boschshcpy import SHCSession, SHCDeviceHelper
-
-# logging.basicConfig(level=logging.DEBUG)
+from boschshcpy import (
+    SHCSession,
+    SHCDeviceHelper,
+    SHCThermostat,
+    SHCBatteryDevice,
+    SHCShutterContact,
+    SHCShutterContact2,
+)
 
 # Create session with additional zeroconf info
 zeroconf = Zeroconf()
 session = SHCSession(
-    controller_ip="192.168.1.6", certificate='../keystore/dev-cert.pem', key='../keystore/dev-key.pem', zeroconf=zeroconf)
+    controller_ip="192.168.1.6",
+    certificate="../keystore/dev-cert.pem",
+    key="../keystore/dev-key.pem",
+    zeroconf=zeroconf,
+)
 zeroconf.close()
 
 for device in session.devices:
@@ -25,6 +34,24 @@ for room in session.rooms:
 for scenario in session.scenarios:
     scenario.summary()
 
+for state in session.userdefinedstates:
+    state.summary()
+
 session.intrusion_system.summary()
 
 session.information.summary()
+
+res = session.rawscan(
+    command="device_service",
+    device_id="hdm:ZigBee:000d6f000b856cb6",
+    service_id="MultiLevelSensor",
+)
+
+device = next(iter(session.device_helper.shutter_contacts))
+print(f"isinstance SHCShutterContact2: {isinstance(device, SHCShutterContact2)}")
+print(f"isinstance SHCShutterContact: {isinstance(device, SHCShutterContact)}")
+print(f"isinstance SHCBatteryDevice: {isinstance(device, SHCBatteryDevice)}")
+
+print(res)
+res = session.rawscan(command="intrusion_detection")
+print(res)
